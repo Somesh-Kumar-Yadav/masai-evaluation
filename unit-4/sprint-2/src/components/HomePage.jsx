1d703e4529623d7aa95e2194cfa4e63d830fb022
Somesh <somesh_nj2_076@masai.school>
2021-08-09 15:39:15 +0530

    4th commit

--- unit-4/sprint-2/src/components/HomePage.jsx
+++ unit-4/sprint-2/src/components/HomePage.jsx
@@ -1,12 +1,27 @@
 import React from "react"
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { getMoviesData } from "../redux/actions";
-import { Nav } from "../styled-compoents/Nav"
+import { Card, Container, Movies, Nav, Show } from "../styled-compoents/Styled-Components"
 export function HomePage() {
     const [search, setSearch] = React.useState("");
+    const [movie, setMovie] = React.useState("");
     const dispatch = useDispatch();
+    React.useEffect(() => {
+     dispatch(getMoviesData());   
+    },[dispatch])
+    const data = useSelector((state) => state.movies);
+    console.log(data);
+    if (movie === "") {
+        setMovie(data[0]);
+    }
+    console.log(movie)
     const handleSearch = () => {
-        dispatch(getMoviesData);
+        const searchData = data.filter((e) => {
+            return e.includes(search);
+        })
+        console.log(searchData);
+            setSearch("");
     }
     return <>
         <Nav>
@@ -14,6 +29,37 @@ export function HomePage() {
                 setSearch(e.target.value);
             }} />
             <button onClick={handleSearch}>Search</button>
-    </Nav>
+        </Nav>
+        <Container>
+            <div>
+                <button>Sort by latest</button>
+                <button>Sort by oldest</button>
+            </div>
+        </Container>
+        <Movies>
+            <div>
+                {data.map((e) => {
+                    return <Card>
+                        <div>
+                        <img src={e.url} alt=""/>
+                        </div>
+                        <div>
+                        <h4><strong>Name : </strong>{e.title}</h4>
+                        <p><strong> Rating : </strong>{e.rating}</p>
+                        <p><strong>Description : </strong>{e.description}</p>
+
+                        <p><strong>Cast : </strong><br/>{e.cast.map((et) => {
+                            return <span>{ et}<br/> </span>
+                        })}</p>
+                        </div>
+                    </Card>
+                })}
+            </div>
+            <div>
+                <Show>
+
+                </Show>
+            </div>
+        </Movies>
     </>
 }
---
+++ unit-4/sprint-2/src/styled-compoents/Styled-Components.jsx
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+export const Nav = styled.div`
+width:100vw;
+height :80px;
+border: 1px solid black;
+display: flex;
+justify-content: center;
+align-items: center;
+&>input{
+    outline: none;
+    width: 400px;
+    border-radius: 20px;
+    height: 40px;
+    padding: 10px;
+    margin: 10px;
+}
+&>button{
+    background-color: green;
+    width: 100px;
+    height: 40px;
+    border: none;
+    color:#fff;
+  padding: 10px;
+    margin: 10px;
+    cursor: pointer;
+    border-radius: 5px;
+}
+&>button:hover{
+    background-color: orange;
+}
+`
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+&>div:nth-child(1){
+    width: 100vw;
+    display: flex;
+    justify-content: space-around;
+}
+&>div:nth-child(1)>button{
+     background-color: green;
+    width: 200px;
+    height: 40px;
+    border: none;
+    color:#fff;
+  padding: 10px;
+    margin: 10px;
+    cursor: pointer;
+    border-radius: 5px;
+}
+&>div:nth-child(1)>button:hover{
+     background-color: orange;
+
+}
+`
+export const Movies = styled.div`
+display: flex;
+`
+export const Show = styled.div`
+
+`
+export const Card = styled.div`
+display: flex;
+width: 50%;
+margin: 20px;
+border: 1px solid black;
+justify-content: center;
+align-items: center;
+padding: 5px;
+border-radius: 20px;
+height: 200px;
+&>div>img{
+width: 100px;
+height: 130px;
+margin: 0 20px;
+}
+`
