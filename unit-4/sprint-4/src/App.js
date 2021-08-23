c5b071dfab9c8ef8c56c4561ab247b0253645934
Somesh <somesh_nj2_076@masai.school>
2021-08-23 14:22:34 +0530

    1st commit

---
+++ unit-4/sprint-4/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { UsdToInr } from "./usdToinr/UsdToInr";
+
+function App() {
+	return (
+		<div className="App">
+			<UsdToInr />
+		</div>
+	);
+}
+
+export default App;
---
+++ unit-4/sprint-4/src/usdToinr/UsdToInr.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const convertToINR = (amount, currency) => {
+    if (currency === "INR") {
+        return (amount / 74.5)+" USD";
+    } else {
+        return amount * 74.5 + " INR";
+    } 
+    }
+export function UsdToInr() {
+    const [value, setValue] = React.useState("");
+    const [currency, setCurrency] = React.useState("INR");
+    const [text,setText ] = React.useState("");
+    const handleConvert = () => {
+       setText(convertToINR(value, currency));
+    }
+    return <>
+        <input type="number" value={value} onChange={(e) => {setValue(e.target.value)}}/>
+        <select value={ currency} onChange={(e)=>{setCurrency(e.target.value)}}>
+            <option>INR</option>
+            <option>USD</option>
+        </select>
+        <button onClick={ handleConvert}>{ currency==="INR"?"Convert to USD":"Convert to INR"}</button>
+        <p>{ text}</p>
+    </>
+}
