869ff5238ec369ec17856422a472c9b9786c8d9f
Somesh <somesh_nj2_076@masai.school>
2021-08-23 15:46:38 +0530

    5th commit

--- unit-4/sprint-4/src/components/HomePage.jsx
+++ unit-4/sprint-4/src/components/HomePage.jsx
@@ -6,6 +6,7 @@ import Container from "@material-ui/core/Container"
 import Card from "@material-ui/core/Card"
 import { CardContent, CardMedia, makeStyles } from "@material-ui/core"
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import {StarIcon} from "@material-ui/icons"
 const useStyles = makeStyles({
     cont: {
         padding:"0.5rem"
@@ -17,14 +18,45 @@ const useStyles = makeStyles({
         marginTop: "1rem",
         width: "80%",
         height: "200px",
-        display: "flex"
+        display: "flex",
+        justifyContent: "space-between",
+        cursor:"pointer"
     },
     image: {
         width: "200px",
         height: "200px"
     },
     content: {
-        
+        display: "flex",
+        flexDirection:"column",
+        justifyContent: "space-between",
+        position: "relative",
+        height: "100%"
+    },
+    icons: {
+        position: "absolute",
+        right:"2rem"
+    },
+    main: {
+        display: "flex",
+        flexDirection:"column",
+        position: "relative",
+        right: "5rem",
+        height: "100%"
+    },
+    bottom: {
+        fontSize: "16px",
+        color: "gray",
+        position: "absolute",
+        bottom: "10px",
+        right:"1rem"
+    },
+    rating: {
+        fontSize: "16px",
+        color: "gray",
+        position: "absolute",
+        bottom: "10px",
+        right:"1rem"
     }
 })
 export default function Home() {
@@ -38,7 +70,7 @@ export default function Home() {
         <Container className={classes.container}>
             <Card className={classes.cards}>
                 <img className={classes.image} src="https://picsum.photos/200" alt="pic" />
-                <CardContent>
+                <CardContent className={classes.main}>
                     <Typography variant="p">
                         Farm Stay in Srirangapatna
                     </Typography>
@@ -48,9 +80,13 @@ export default function Home() {
                     <Typography variant="p">
                         4 guests , 2 bedroom , 3 beds , 3 bathrooms
                     </Typography>
+                    <Typography calssName={classes.rating} variant="p">
+                        <StarIcon></StarIcon> 4.8 (43 reviews) 
+                    </Typography>
                 </CardContent>
                 <CardContent className={classes.content}>
-                        <FavoriteIcon/>
+                    <FavoriteIcon className={classes.icons} />
+                    <Typography className={classes.bottom} variant="h6">2900INR/night</Typography>
                 </CardContent>
              </Card>
             <Card className={classes.cards}>
---
+++ unit-4/sprint-4/src/data.js
@@ -0,0 +1,7 @@
+export const data = [
+	{
+		id: 1,
+		title: "Rusting Nest - Farm Stay for Cycling weekend.",
+		subtitle: "Farm Stay in Srirangapatna",
+	},
+];
