38d2e52080321891672b83cb4ef502344ec8f877
Somesh <somesh_nj2_076@masai.school>
2021-09-27 15:20:31 +0530

    4th commit

--- unit-5/sprint-4/src/controllers/user.controller.js
+++ unit-5/sprint-4/src/controllers/user.controller.js
@@ -1,20 +1,42 @@
 const express = require("express");
 
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/user.model");
 
 const router = express.Router();
 
 const upload = require("../utils/fileUploads");
 
+const newToken = (user) => {
+	return jwt.sign({ user: user }, "Somesh");
+};
+
 router.post("/", upload.single("profile_photo_url"), async (req, res) => {
-	const user = await User.create({
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password,
-		profile_photo_url: req.file.path,
-		roles: req.body.roles,
-	});
-	return res.status(201).json({ user });
+	let user;
+	try {
+		user = await User.findOne({ email: req.body.email }).lean().exec();
+		if (user) {
+			return res.status(400).send({
+				status: "failed",
+				message: "Try different email or password",
+			});
+		}
+		user = await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password,
+			profile_photo_url: req.file.path,
+			roles: req.body.roles,
+		});
+		const token = newToken(user);
+
+		return res.status(201).json({ token });
+	} catch (e) {
+		return res
+			.status(400)
+			.send({ status: "failed", message: "Something went wrong" });
+	}
 });
 
 router.get("/", async (req, res) => {
