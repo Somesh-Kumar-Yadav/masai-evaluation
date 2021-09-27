4289c0b7d4b0223872bacbfeecfc780e82e95901
Somesh <somesh_nj2_076@masai.school>
2021-09-27 14:22:49 +0530

    1st commit

---
+++ unit-5/sprint-4/src/models/lecture.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		email: { type: String, required: true },
+		password: { type: String, required: true },
+		profile_photo_url: { type: String, required: true },
+		roles: [{ type: String, required: true }],
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+	}
+);
